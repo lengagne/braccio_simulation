a06ec57d5c9c56e87bb5f2d0214304e45cf3ec5b
Sébastien Lengagne <lengagne@gmail.com>
2021-03-11 09:45:54 +0100

    add robot with camera

--- braccio_arm_gazebo/src/VectorInput.cpp
+++ braccio_arm_gazebo/src/VectorInput.cpp
@@ -20,7 +20,7 @@
 #include "std_msgs/Float64.h"
 #include "std_msgs/Float64MultiArray.h"
 
-std_msgs::Float64 msg_base, msg_elbow, msg_gripper, msg_shoulder, msg_wrist_pitch, msg_wrist_roll;
+std_msgs::Float64 msg_base, msg_elbow, msg_gripper, msg_shoulder, msg_wrist_pitch, msg_wrist_roll, msg_camera_rotate, msg_camera_depth, msg_camera_height, msg_camera_tilt;
 
 void callback(const std_msgs::Float64MultiArray& msg)
 {
@@ -32,6 +32,15 @@ void callback(const std_msgs::Float64MultiArray& msg)
     msg_gripper.data = msg.data[5];
 }
 
+void callback_camera(const std_msgs::Float64MultiArray& msg)
+{
+    msg_camera_rotate.data = msg.data[0];
+    msg_camera_depth.data = msg.data[1];
+    msg_camera_height.data = msg.data[2];
+    msg_camera_tilt.data = msg.data[3];
+}
+
+
 int main(int argc, char *argv[])
 {
     msg_base.data = 1.57; 
@@ -40,6 +49,11 @@ int main(int argc, char *argv[])
     msg_shoulder.data = 1.57; 
     msg_wrist_pitch.data = 1.57; 
     msg_wrist_roll.data = 1.57; 
+    
+    msg_camera_rotate.data = 0.0;
+    msg_camera_depth.data = 1.0;
+    msg_camera_height.data = 0.5;
+    msg_camera_tilt.data = -0.2;
     
     ros::init(argc, argv, "braccio_arm_vector_input");
 
@@ -53,7 +67,14 @@ int main(int argc, char *argv[])
     ros::Publisher pub_wrist_pitch = nh.advertise<std_msgs::Float64>("/braccio_arm/wrist_pitch_joint_position_controller/command", 1000);
     ros::Publisher pub_wrist_roll = nh.advertise<std_msgs::Float64>("/braccio_arm/wrist_roll_joint_position_controller/command", 1000);
     
+    ros::Publisher pub_camera_rotate = nh.advertise<std_msgs::Float64>("/braccio_arm/camera_rotate_controller/command", 1000);
+    ros::Publisher pub_camera_height = nh.advertise<std_msgs::Float64>("/braccio_arm/camera_height_controller/command", 1000);
+    ros::Publisher pub_camera_depth = nh.advertise<std_msgs::Float64>("/braccio_arm/camera_depth_controller/command", 1000);
+    ros::Publisher pub_camera_tilt = nh.advertise<std_msgs::Float64>("/braccio_arm/camera_tilt_controller/command", 1000);
+    
     ros::Subscriber sub = nh.subscribe("/braccio_arm/joint_angles", 10, callback);
+    
+    ros::Subscriber sub_camera = nh.subscribe("/braccio_arm/camera_joint_angles", 10, callback_camera);
     
     ros::Rate rate(100.0);
     while (nh.ok())
@@ -65,6 +86,12 @@ int main(int argc, char *argv[])
         pub_shoulder.publish(msg_shoulder);
         pub_wrist_pitch.publish(msg_wrist_pitch);
         pub_wrist_roll.publish(msg_wrist_roll);
+        
+        pub_camera_rotate.publish(msg_camera_rotate);
+        pub_camera_height.publish(msg_camera_height);
+        pub_camera_depth.publish(msg_camera_depth);
+        pub_camera_tilt.publish(msg_camera_tilt);
+        
         ros::spinOnce();
         rate.sleep();
     }    
